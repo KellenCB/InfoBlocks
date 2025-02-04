a5f791a588cad0e8cc5f5ec72badc667ffd3f7e2
KellenCB <kellen.cb@hotmail.com>
2025-02-04 22:12:42 +0000

    Create tagConfig.js

---
+++ tagConfig.js
@@ -0,0 +1,37 @@
+export const tagConfig = {
+    partType: {
+        label: "Part Type",
+        containerId: "partType_tags_list",
+        overlayId: "partType_tags_overlay",
+        editOverlayId: "partType_tags_edit_overlay",
+        tags: ["Helm", "Torso", "Left arm", "Right arm", "Legs", "Boots", "Other"]
+    },
+    actionType: {
+        label: "Action Type",
+        containerId: "actionType_tags_list",
+        overlayId: "actionType_tags_overlay",
+        editOverlayId: "actionType_tags_edit_overlay",
+        tags: ["Action", "Reaction", "Bonus action", "Free action"]
+    },
+    abilityType: {
+        label: "Ability Type",
+        containerId: "abilityType_tags_list",
+        overlayId: "abilityType_tags_overlay",
+        editOverlayId: "abilityType_tags_edit_overlay",
+        tags: ["Damage", "Buff", "Debuff", "Heal", "Movement", "Ranged", "Melee", "Spell"]
+    },
+    modifierType: {
+        label: "Modifier Type",
+        containerId: "modifierType_tags_list",
+        overlayId: "modifierType_tags_overlay",
+        editOverlayId: "modifierType_tags_edit_overlay",
+        tags: ["STR", "DEX", "CON", "INT", "WIS", "CHA"]
+    },
+    damageType: {
+        label: "Damage Type",
+        containerId: "damageType_tags_list",
+        overlayId: "damageType_tags_overlay",
+        editOverlayId: "damageType_tags_edit_overlay",
+        tags: ["Acid", "Bludgeoning", "Cold", "Fire", "Force", "Lightning", "Necrotic", "Piercing", "Poison", "Psychic", "Radiant", "Slashing", "Thunder"]
+    }
+};
